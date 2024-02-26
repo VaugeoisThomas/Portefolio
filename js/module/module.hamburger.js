826728ac902858766ddc5e12a170296d29088516
Thomas <vaugeois.thomas@gmail.com>
2024-02-26 10:40:02 +0100

    Fix header on small screen

--- js/module/module.hamburger.js
+++ js/module/module.hamburger.js
@@ -1,9 +1,11 @@
 export function hamburger(){
     const menuHamburger = document.querySelector('.menu-hamburger');
     const navLinks = document.querySelector('.nav-links');
+    const main = document.querySelector('.main');
     
     menuHamburger.addEventListener('click', () => {
         navLinks.classList.toggle('is-opened');
         menuHamburger.classList.toggle('is-opened');
+        main.classList.toggle('is-opened');
     });
 } 
