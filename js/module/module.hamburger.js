9e5407673cbbbc737043e8c7c3c9660a6e8dbda5
Thomas <vaugeois.thomas@gmail.com>
2024-03-04 18:01:30 +0100

    Add : Toggle class 'is-opened' for each navbar a

--- js/module/module.hamburger.js
+++ js/module/module.hamburger.js
@@ -1,9 +1,18 @@
 export function hamburger(){
     const menuHamburger = document.querySelector('.menu-hamburger');
     const navbar = document.querySelector('.navbar');
+    const links = document.querySelectorAll('.navbar a');
+
     
     menuHamburger.addEventListener('click', () => {
         menuHamburger.classList.toggle('is-opened');
         navbar.classList.toggle('is-opened');
+    });
+
+    links.forEach(link => {
+        link.addEventListener('click', () => {
+            menuHamburger.classList.toggle('is-opened');
+            navbar.classList.remove('is-opened');
+        })
     });
 } 
